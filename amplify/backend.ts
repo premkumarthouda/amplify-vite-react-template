44c7f5a5c33e445cd493c522c04640229b3d8e13
premkumarthouda <113364219+premkumarthouda@users.noreply.github.com>
2024-06-26 20:30:37 +0530

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -7,6 +7,12 @@ const backend = defineBackend({
   data,
 })
 
+const { groups } = backend.auth.resources
+
+
+// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_iam.IRole.html
+groups["ADMINS"].role
+
 const { cfnUserPool } = backend.auth.resources.cfnResources
 // an empty array denotes "email" and "phone_number" cannot be used as a username
 cfnUserPool.usernameAttributes = []
