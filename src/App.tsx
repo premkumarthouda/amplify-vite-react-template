3d1171f799a0fa9d0710d002c9b1ea97f5dac766
premkumarthouda <113364219+premkumarthouda@users.noreply.github.com>
2024-06-27 20:31:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@aws-amplify/ui-react/styles.css'
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { signUp } from "aws-amplify/auth"
 
 const client = generateClient<Schema>();
 
